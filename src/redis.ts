2df9d2411353893566d3f5bee51dbd90649ba97a
priyanshu1976 <priyanshu.cto@gmail.com>
2025-11-21 23:12:45 +0530

    updated varaible secrets

--- src/redis.ts
+++ src/redis.ts
@@ -4,6 +4,6 @@ export const redis = new IORedis({
   host: process.env.REDIS_HOST, // e.g., "redis-14758.c256.us-east-1-2.ec2.redns.redis-cloud.com"
   port: Number(process.env.REDIS_PORT), // e.g., 14758
   password: process.env.REDIS_PASSWORD,
-  maxRetriesPerRequest: null, // <-- this is required by BullMQ v5
+  maxRetriesPerRequest: null, // <-- this is required by BullM
   enableReadyCheck: true,
 })
